9a1f2b1f7f53392ed6c34709d91d68f7a576bb9c
Eduardo <euodeionomedeusuario@gmail.com>
2018-05-05 12:36:51 -0300

    implementing send_messages method

--- laws/__init__.py
+++ laws/__init__.py
@@ -1,5 +1,20 @@
 from flask import Flask
+from flask_mail import Mail
 
 app = Flask(__name__)
+
+app.config.update(
+        DEBUG=True,
+        #EMAIL SETTINGS
+        MAIL_SERVER='smtp.gmail.com',
+        MAIL_PORT=465,
+        MAIL_USE_SSL=True,
+        MAIL_USERNAME = 'lawsclassroom@gmail.com',
+        MAIL_PASSWORD = '@lawsclassroom'
+        )
+        
+app.config["SECRET_KEY"] = "SECRET_KEY"
+
+mail=Mail(app)
 
 from laws.controllers import routes
--- laws/controllers/routes.py
+++ laws/controllers/routes.py
@@ -1,5 +1,8 @@
 from flask import render_template, redirect, request
 
+from flask_mail import Message
+from laws import mail
+
 from laws import app
 
 @app.route("/")
@@ -13,8 +16,11 @@ def send_message():
 	phone = request.form.get("phone")
 	message = request.form.get("message")
 
-	print(email)
-	print(phone)
-	print(message)
-
+	msg = Message(
+		'Mensagem no Site LAWS',
+		sender='lawsclassroom@gmail.com',
+		recipients=
+		['lawsclassroom@gmail.com'])
+	msg.html="E-mail - {0}<br>Telefone - {1}<br>Mensagem<br>{2}".format(email, phone, message)
+	mail.send(msg)
 	return redirect("/")
