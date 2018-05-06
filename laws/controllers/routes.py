7d90e16e8bf8b15e5e3dd1e0db88bf2ffe11839c
Eduardo <euodeionomedeusuario@gmail.com>
2018-05-05 22:26:11 -0300

    implementing register subscriber method

--- laws/controllers/routes.py
+++ laws/controllers/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, redirect, request
 
 from flask_mail import Message
 from laws import mail
+from laws import db
 
 from laws import app
 
@@ -10,7 +11,7 @@ def index():
 	return render_template("index.html")
 
 
-@app.route("/messages/", methods=["post"])
+@app.route("/messages/", methods=["POST"])
 def send_message():
 	email = request.form.get("email")
 	phone = request.form.get("phone")
@@ -23,4 +24,14 @@ def send_message():
 		['lawsclassroom@gmail.com'])
 	msg.html="E-mail - {0}<br>Telefone - {1}<br>Mensagem<br>{2}".format(email, phone, message)
 	mail.send(msg)
+	return redirect("/")
+
+
+@app.route("/subscribers/", methods=["POST"])
+def register_subscribers():
+	name = request.form.get("name")
+	email = request.form.get("email")
+
+	db.subscribers.insert({"name": name, "email": email})
+
 	return redirect("/")
