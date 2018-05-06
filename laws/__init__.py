50231c7d035856765b59707844125f41f4eadfe3
Eduardo <euodeionomedeusuario@gmail.com>
2018-05-05 22:20:28 -0300

    adding conector bd

--- laws/__init__.py
+++ laws/__init__.py
@@ -1,20 +1,24 @@
 from flask import Flask
 from flask_mail import Mail
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
 app.config.update(
         DEBUG=True,
-        #EMAIL SETTINGS
         MAIL_SERVER='smtp.gmail.com',
         MAIL_PORT=465,
         MAIL_USE_SSL=True,
         MAIL_USERNAME = 'lawsclassroom@gmail.com',
         MAIL_PASSWORD = '@lawsclassroom'
         )
-        
+
 app.config["SECRET_KEY"] = "SECRET_KEY"
 
 mail=Mail(app)
+
+client = MongoClient('mongodb://localhost:27017/')
+
+db = client.lawssitedb
 
 from laws.controllers import routes
